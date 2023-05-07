e9bb6a4db0df34d49df84fd320a6c02fbdd51740
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2023-05-07 23:05:19 +0300

    Everything is complete

---
+++ README.md
@@ -0,0 +1,7 @@
+This is CLI app. There are several functions for work with contacts.
+Commands execution result shown in pictures:
+
+https://monosnap.com/file/7NckCpjoKVsfngZWo1OLd0LLxi1Glr
+https://monosnap.com/file/xyYFKqkpgMEBZbuIRlCVLTvNPvyXFg
+https://monosnap.com/file/B0m4eYUPSPBrT3ZMTxt2KWP2KNYvCo
+https://monosnap.com/file/6BUNbNlLmfMR4dvISfz7gtEVF7tKL9
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hiiiiii crown twice");
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getById(id);
+      return console.log(oneContact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+      break;
+    case "remove":
+      const deleteBook = await contacts.deleteById(id);
+      return console.log(deleteBook);
+      break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+
+invokeAction(argv);
